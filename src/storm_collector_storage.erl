af279268856ea8bd1890b82c850432708733f447
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-09-03 12:38:21 +0100

    Uses ets to cache datapoints in memory.

--- src/storm_collector_storage.erl
+++ src/storm_collector_storage.erl
@@ -7,7 +7,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {vent, items=[]}).
+-record(state, {vent, items=[], cache=[]}).
 
 -define(DATA_FILE, "data/weather_datapoints.dets").
 
@@ -16,10 +16,12 @@ start_link() ->
 
 init([]) ->
   {ok, WeatherTable} = dets:open_file(weather_data, [{file, ?DATA_FILE}, {type, set}]),
+  WeatherCache = ets:new(weather_cache, [set]),
+  true = ets:from_dets(WeatherCache, WeatherTable),
 
   {ok, VentPid} = gen_event:start_link(),
   gen_event:add_handler(VentPid, log_handler, []),
-  State = #state{vent=VentPid, items=WeatherTable},
+  State = #state{vent=VentPid, items=WeatherTable, cache=WeatherCache},
   {ok, State}.
 
 add(BinaryData) ->
@@ -29,18 +31,19 @@ all() ->
   gen_server:call(?MODULE, all).
 
 %% callbacks
-handle_cast({add, BinaryData}, State=#state{vent=VentPid, items=WeatherTable}) ->
+handle_cast({add, BinaryData}, State=#state{vent=VentPid, items=WeatherTable, cache=WeatherCache}) ->
   Item = process_binary(BinaryData),
   ItemWithMetadata = add_metadata_to_item(Item),
   Uuid = maps:get(uuid, ItemWithMetadata),
+  ets:insert(WeatherCache, {Uuid, ItemWithMetadata}),
   dets:insert(WeatherTable, {Uuid, ItemWithMetadata}),
   gen_event:notify(VentPid, {datapoint, ItemWithMetadata}),
   {noreply, State}.
 
-handle_call(all, _From, State = #state{vent=_VentPid, items=WeatherTable}) ->
-  Items = dets:foldl(fun({_Uuid, Item}, Acc) ->
+handle_call(all, _From, State = #state{vent=_VentPid, cache=WeatherCache}) ->
+  Items = ets:foldl(fun({_Uuid, Item}, Acc) ->
    [Item | Acc]
-  end, [], WeatherTable),
+  end, [], WeatherCache),
   {reply, Items, State}.
 
 handle_info(_Info, State) ->
