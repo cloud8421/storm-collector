4aea04a8df53910f9837eecf38f690020be01f0b
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-09-04 22:13:15 +0100

    Move port to module attribute.

--- src/storm_collector_server.erl
+++ src/storm_collector_server.erl
@@ -7,11 +7,13 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-define(PORT, 5678).
+
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-  {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0}, {active, true}]),
+  {ok, LSock} = gen_tcp:listen(?PORT, [binary, {packet, 0}, {active, true}]),
   {ok, LSock, 0}.
 
 %% callbacks
